1444da4849ada8331ad362e5347a17e18b39f954
Maxime Papin <maxime.papin@b-com.com>
2023-03-29 12:07:08 +0200

    fix: add missing namespace

--- Runtime/PlaygroundPrefabsScriptableObject.cs
+++ Runtime/PlaygroundPrefabsScriptableObject.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PrefabList", menuName = "SharedPlayground/PlaygroundPrefabs")]
-public class PlaygroundPrefabsScriptableObject : ScriptableObject
+namespace Bcom.SharedPlayground
 {
-    public List<GameObject> prefabs;
+    [CreateAssetMenu(fileName = "PrefabList", menuName = "SharedPlayground/PlaygroundPrefabs")]
+    public class PlaygroundPrefabsScriptableObject : ScriptableObject
+    {
+        public List<GameObject> prefabs;
+    }
 }
